7757d1ab4cec31f7563132036a6ddfdf06022485
Igor Katson <igor_katson@symantec.com>
2019-01-02 15:06:49 -0800

    Texture filtering

--- src/texture.ts
+++ src/texture.ts
@@ -57,6 +57,8 @@ export class Texture {
     private bind(gl: WebGL2RenderingContext, img: HTMLImageElement) {
         gl.bindTexture(gl.TEXTURE_2D, this.texture);
         gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
         gl.generateMipmap(gl.TEXTURE_2D);
     }
 
