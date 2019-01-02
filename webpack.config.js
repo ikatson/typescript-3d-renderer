6836855faaa3ace0761e6350f23e69785c380f3a
Igor Katson <igor_katson@symantec.com>
2019-01-02 10:06:13 -0800

    Trying to make things more compact

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    optimization: {usedExports: true, sideEffects: true},
     mode: 'production',
     watch: true
 };
