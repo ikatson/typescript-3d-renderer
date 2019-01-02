8cd5e7a8898582fc15c41dee531f0578bcc8dae4
Igor Katson <igor_katson@symantec.com>
2019-01-02 15:50:56 +0000

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.ts',
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                // exclude: /node_modules/,
+                options: {
+                    // transpileOnly: true
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    watch: true
+};
